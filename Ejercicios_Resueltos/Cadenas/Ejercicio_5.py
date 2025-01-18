89c78cb518ba86b6c47afb5f2efa3f144d2c182c
GerssonDev <gersondlacruz10@gmail.com>
2025-01-18 14:33:35 -0500

    Ejercicios de cadenas 3,4 & 5

---
+++ Ejercicios_Resueltos/Cadenas/Ejercicio_5.py
@@ -0,0 +1,7 @@
+"""
+Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida.
+"""
+
+frase = str(input("Intrudicir una frase: "))
+
+print(f'{frase[::-1]}')
